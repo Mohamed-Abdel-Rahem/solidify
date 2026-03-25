98a1fb10474745c376729f923fd7a923289d36a2
Mohamed-Abdel-Rahem <169306620+Mohamed-Abdel-Rahem@users.noreply.github.com>
2026-03-25 05:46:16 +0200

    add files domain

---
+++ lib/features/user/domain/entities/user_entities.dart
@@ -0,0 +1,17 @@
+class UserEntities {
+  final String name;
+  final String phone;
+  final String email;
+}
+
+class AddressEntities {
+  final String street;
+  final String suite;
+  final String city;
+  final String zipcode;
+}
+
+class GeoEntities {
+  final String lat;
+  final String lng;
+}
